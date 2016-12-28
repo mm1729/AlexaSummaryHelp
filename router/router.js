2abf2c26b6bb859bb3fee9fa7e63115dd38817af
Moulindra Muchumari <moulindra1729@gmail.com>
2016-12-28 10:38:59 -0500

    Initial server

--- router/router.js
+++ router/router.js
@@ -3,5 +3,9 @@ var path = require('path')
 var router = express.Router()
 
 router.get('/', function(req, res) {
+  console.log(__dirname)
   res.sendFile('index.html', {'root' : path.join(__dirname, '../public/html/')})
 })
+
+
+module.exports = router

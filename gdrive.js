47cb9368be8418a5d9f38b44f08c5b3e6be1e147
Moulindra Muchumari <moulindra1729@gmail.com>
2016-12-29 13:15:39 -0500

    Created signup, redirect and token processing

---
+++ gdrive.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var gdrive = {};
+var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+var credentials = {};
+
+// Initialize credentials
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  content = JSON.parse(content);
+  credentials.clientSecret = content.web.client_secret;
+  credentials.clientId = content.web.client_id;
+  credentials.redirectUrl = content.web.redirect_uris[0];
+});
+
+// authenticate the authorization code to get an access token
+// @return {token, error} -> error is true and token in null if there is an error, else error is false
+gdrive.authenticate = function(code, callback) {
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(credentials.clientId, credentials.clientSecret, credentials.redirectUrl);
+
+  oauth2Client.getToken(code, function(err, token) {
+    if (err) {
+      callback(null, true);
+      return;
+    }
+    oauth2Client.credentials = token;
+    callback(token, false);
+  });
+}
+
+// gets the user name of the token
+// @return {username, error} -> error is true and username is null if there is an error, else error is false
+gdrive.getUserName = function(token, callback) {
+  var service = google.drive('v3');
+  service.about.get({
+    auth: token
+  }, function(err, response) {
+    if(err) {
+      callback(null, true);
+      return;
+    } else {
+      callback(response, false);
+    }
+  })
+}
--- router/router.js
+++ router/router.js
@@ -1,15 +1,37 @@
 var express = require('express')
 var path = require('path')
 var router = express.Router()
+var gdrive = require('../gdrive.js')
+
 
 router.get('/', function(req, res) {
-  console.log(__dirname)
-  res.sendFile('index.html', {'root' : path.join(__dirname, '../public/html/')})
+  res.sendFile('signup.html', {'root' : path.join(__dirname, '../public/html/')})
 })
 
 router.get('/oauth2callback', function(req, res) {
-  console.log(req)
-  res.send("got oauth2")
+  var code = req.query.code
+  if(!code) {
+    res.send("We encountered an error authenticating your Google Drive account. Please try again.")
+  }
+  gdrive.authenticate(code, function(token, err) {
+    if(err === true) {
+      res.send("We encountered an error authenticating your Google Drive account. Please try again.")
+    } else {
+      gdrive.getUserName(token, function(username, err) {
+        if(err === true) {
+          res.send("We encountered an error authenticating your Google Drive account. Please try again.")
+        } else {
+          res.send(username)
+        }
+      })
+    }
+  })
 })
+
+router.get('/signup', function(req, res) {
+  res.sendFile('signup.html', {'root' : path.join(__dirname, '../public/html/')})
+})
+
+router.get('/')
 
 module.exports = router
